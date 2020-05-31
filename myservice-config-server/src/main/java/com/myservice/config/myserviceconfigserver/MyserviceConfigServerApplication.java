1c2b52d4284de8f672106851dac202a12456cb5b
jrsilvanio@gmail.com <Lisboa18>
2020-05-31 13:09:02 +0100

    Start config server

---
+++ myservice-config-server/src/main/java/com/myservice/config/myserviceconfigserver/MyserviceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.myservice.config.myserviceconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyserviceConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyserviceConfigServerApplication.class, args);
+	}
+
+}
